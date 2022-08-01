413a8d1907636a4609b614afa39bf216b7fdeef8
Kevin Galligan <kgalligan@gmail.com>
2022-08-01 18:14:37 -0400

    Simple encryption setup

--- shared/src/iosMain/kotlin/co/touchlab/kampkit/KoinIOS.kt
+++ shared/src/iosMain/kotlin/co/touchlab/kampkit/KoinIOS.kt
@@ -3,12 +3,6 @@ package co.touchlab.kampkit
 import co.touchlab.kampkit.db.KaMPKitDb
 import co.touchlab.kermit.Logger
 import co.touchlab.sqliter.DatabaseConfiguration
-import co.touchlab.sqliter.DatabaseFileContext
-import co.touchlab.sqliter.NO_VERSION_CHECK
-import co.touchlab.sqliter.createDatabaseManager
-import co.touchlab.sqliter.databaseFileExists
-import co.touchlab.sqliter.getVersion
-import co.touchlab.sqliter.setVersion
 import com.russhwolf.settings.AppleSettings
 import com.russhwolf.settings.Settings
 import com.squareup.sqldelight.db.SqlDriver
@@ -41,55 +35,9 @@ actual val platformModule = module {
     single { Darwin.create() }
 }
 
-private fun Module.createClearDb() {
-    single<SqlDriver> { NativeSqliteDriver(KaMPKitDb.Schema, "KampkitDb") }
-}
-
 private fun Module.createEncryptedDb() {
     val unsafeEncryptionKey = "abacab"
-    val clearDbName = "KampkitDb"
     val encryptedDbName = "KampkitDb2"
-
-    if (DatabaseFileContext.databaseFileExists(clearDbName)) {
-        val fromManager = createDatabaseManager(DatabaseConfiguration(
-            name = clearDbName,
-            version = NO_VERSION_CHECK,
-            create = {}
-        ))
-
-        val toManager = createDatabaseManager(
-            DatabaseConfiguration(
-                name = encryptedDbName,
-                version = NO_VERSION_CHECK,
-                create = {},
-                encryptionConfig = DatabaseConfiguration.Encryption(unsafeEncryptionKey)
-            )
-        )
-
-        //Force-create the target db
-        toManager.createMultiThreadedConnection().close()
-
-        val fromConn = fromManager.createMultiThreadedConnection()
-
-        val encryptedDbPath = DatabaseFileContext.databasePath(
-            encryptedDbName,
-            null
-        )
-
-        fromConn.rawExecSql(
-            "ATTACH DATABASE '$encryptedDbPath' AS encrypted KEY '${unsafeEncryptionKey}'"
-        )
-        fromConn.rawExecSql("select sqlcipher_export('encrypted')")
-        fromConn.rawExecSql("DETACH DATABASE encrypted")
-
-        //Don't forget the version!!!
-        toManager.createMultiThreadedConnection().apply {
-            setVersion(fromConn.getVersion())
-            close()
-        }
-
-        DatabaseFileContext.deleteDatabase(clearDbName, null)
-    }
 
     val dbConfig = DatabaseConfiguration(
         name = encryptedDbName,
